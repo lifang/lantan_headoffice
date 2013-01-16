701fe96e4c38e6239310e0543df05c03ffbe1825
qianjun <er6788@126.com>
2013-01-16 13:34:47 +0800

    qianjun

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LantanHeadoffice::Application.config.secret_token = '1202d523780d75be0e2f5c49d0669320df4c8fd861df3f3814194db5aa3d8069aa61e15035539b157ca49524707769b4dd0c4abb958cacabd6e38034bb149fc6'
