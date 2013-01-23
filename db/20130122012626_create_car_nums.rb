8be074faaca3d5338c6ad1964d046360f9b86de3
qianjun <er6788@126.com>
2013-01-23 15:30:00 +0800

    qianjun

--- db/20130122012626_create_car_nums.rb
+++
@@ -1,9 +0,0 @@
-class CreateCarNums < ActiveRecord::Migration
-  def change
-    create_table :car_nums do |t|
-      t.integer :num
-      t.integer :car_model_id
-      t.timestamps
-    end
-  end
-end
