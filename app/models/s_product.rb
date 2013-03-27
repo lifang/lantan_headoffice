b28f25578ccee8fbbfa2c8bf3fa16ca9deeaa2ef
cloud <cloud@cloud-Sony.(none)>
2013-03-27 18:34:29 +0800

    use lantan store db

---
+++ app/models/s_product.rb
@@ -0,0 +1,5 @@
+#encoding: utf-8
+class SProduct < ActiveRecord::Base
+  set_table_name :"lantan_store.products"
+  set_primary_key "id"
+end
--- app/models/sync.rb
+++ app/models/sync.rb
@@ -18,8 +18,8 @@ class Sync < ActiveRecord::Base
   end
 
   #发送上传请求
-  def self.send_file(store_id,file_url,filename)
-    query={:store_id=>store_id}
+  def self.send_file(file_url,filename)
+    query={}
     url = URI.parse Constant::HEAD_OFFICE
     File.open(file_url) do |file|
       req = Net::HTTP::Post::Multipart.new url.path,query.merge!("upload" => UploadIO.new(file, "application/zip", "#{filename}"))
@@ -74,7 +74,7 @@ class Sync < ActiveRecord::Base
 
 
   def self.out_data
-    models=['product.rb', 'new.rb', 'car_model.rb']
+    models=['s_product.rb', 's_sale.rb', 's_car_model.rb']
     path="#{Rails.root}/public/"
     dirs=["syncs_datas/","#{Time.now.strftime("%Y-%m").to_s}/","#{Time.now.strftime("%Y-%m-%d").to_s}/"]
     dirs.each_with_index {|dir,index| Dir.mkdir path+dirs[0..index].join   unless File.directory? path+dirs[0..index].join }
@@ -84,10 +84,12 @@ class Sync < ActiveRecord::Base
         cap = eval(model_name.split("_").inject(String.new){|str,name| str + name.capitalize})
         attrs = cap.where("TO_DAYS(NOW())-TO_DAYS(created_at)=1")
         unless attrs.blank?
-          file = File.open("#{path+dirs.join+model_name}.log","w+")
-          file.write("#{cap.column_names.join(";||;")}\r\n|::|")
+          name_arr = model_name.split('_')
+          name_arr.delete_at(0)
+          file = File.open("#{path+dirs.join+name_arr.join('_')}.log","w+")
+          file.write("#{cap.column_names.join(";||;")}\n\n|::|")
           file.write("#{attrs.inject(String.new) {|str,attr| 
-            str+attr.attributes.values.join(";||;").gsub(";||;true;||;",";||;1;||;").gsub(";||;false;||;",";||;0;||;")+"\r\n|::|"}}")
+            str+attr.attributes.values.join(";||;").gsub(";||;true;||;",";||;1;||;").gsub(";||;false;||;",";||;0;||;")+"\n\n|::|"}}")
           file.close
         end
       end
