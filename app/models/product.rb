b395d22071cf08429528f408f32c7cec9c5313f7
Luping <165340815@qq.com>
2013-04-01 15:52:02 +0800

    luping04012

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,7 @@ class Product < ActiveRecord::Base
   has_many :prod_mat_relations
   has_many :svcard_prod_relations
   belongs_to :store
+  has_many :image_urls
   PRODUCT_TYPES = {0 => "汽车清洁用品", 1 => "汽车美容用品", 2 => "汽车装饰产品", 3 => "汽车配件产品", 4 => "汽车电子产品",
     5 => "清洗服务", 6 => "维修服务", 7 => "钣喷服务", 8 => "美容服务", 9 => "安装服务"} #产品类别
   PROD_TYPES = {:PRODUCT =>0,:SERVICE =>1}  #0 为产品 1 为服务
