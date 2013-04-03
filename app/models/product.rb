c5d96b51765d87c6a64da8209ca64c52c41ed9e7
lifang <santhe_2004@126.com>
2013-04-03 10:27:50 +0800

    updte product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,9 +9,9 @@ class Product < ActiveRecord::Base
   has_many :svcard_prod_relations
   belongs_to :store
   has_many :image_urls
-  PRODUCT_TYPES = {0 => "汽车清洁用品", 1 => "汽车美容用品", 2 => "汽车装饰产品", 3 => "汽车配件产品", 4 => "汽车电子产品",
-    5 => "清洗服务", 6 => "维修服务", 7 => "钣喷服务", 8 => "美容服务", 9 => "安装服务", 10 => "其他"} #产品类别
-    PRODUCT_END=5
+  PRODUCT_TYPES = {0 => "汽车清洁用品", 1 => "汽车美容用品", 2 => "汽车装饰产品", 3 => "汽车配件产品", 4 => "汽车电子产品",5 =>"其他产品",
+    6 => "清洗服务", 7 => "维修服务", 8 => "钣喷服务", 9 => "美容服务", 10 => "安装服务", 11 => "其他服务"} #产品类别
+    PRODUCT_END = 6
   PROD_TYPES = {:PRODUCT =>0,:SERVICE =>1}  #0 为产品 1 为服务
   IS_VALIDATE={:NO=>0,:YES=>1} #0 无效 已删除状态 1 有效
 end
