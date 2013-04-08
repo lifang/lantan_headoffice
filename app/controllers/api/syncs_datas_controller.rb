d5ec6d3ab6726f727291b0936549bec3449ef862
lifang <santhe_2004@126.com>
2013-04-08 16:03:41 +0800

    update detial

--- app/controllers/api/syncs_datas_controller.rb
+++ app/controllers/api/syncs_datas_controller.rb
@@ -7,14 +7,35 @@ class Api::SyncsDatasController < ApplicationController
     dirs=["syncs_data","/#{Time.now.strftime("%Y-%m").to_s}","/#{Time.now.strftime("%Y-%m-%d").to_s}"]
     dirs.each_with_index {|dir,index| Dir.mkdir path+dirs[0..index].join unless File.directory? path+dirs[0..index].join }
     filename = params[:url].original_filename
-    File.open(path+dirs.join("")+"/"+filename, "wb")  {|f|  f.write(params[:url].read) }
+    path = path + dirs.join("")
+    if filename.include? "zip"
+      hour_dir = Time.now.hour >= 10 ? "/#{Time.now.hour}" : "/0#{Time.now.hour}"
+      path = path + hour_dir
+      Dir.mkdir path unless File.directory? path
+    end
+    File.open(path + "/"+filename, "wb")  {|f|  f.write(params[:url].read) }
     render :text=>"success"
   end
 
+  #门店同步图片到总部
   def syncs_pics
-    path="#{Rails.root}/public/"
+    path="#{Rails.root}/public"
+    realy_dir = ""
+    header_params = params[:url].headers.split(";")
+    header_params.each do |p|
+      if p.include? "path="
+        realy_dir = p.split("path=")[1]
+        break
+      end
+    end
+    dirs = realy_dir.gsub("%2F", "/").gsub("\"", "").split("/") unless realy_dir.empty?
+    dirs.delete("")
+    dirs.each_with_index { |dir,index|
+      Dir.mkdir path+"/"+dirs[0..index].join("/") unless File.directory? path+"/"+dirs[0..index].join("/")
+    } unless dirs.blank?
     filename = params[:url].original_filename
-    File.open(path+filename, "wb")  {|f|  f.write(params[:url].read) }
+    path = path + "/" + dirs.join("/") unless dirs.blank?
+    File.open(path+"/"+filename, "wb")  {|f|  f.write(params[:url].read) }
     render :text=>"success"
   end
   
