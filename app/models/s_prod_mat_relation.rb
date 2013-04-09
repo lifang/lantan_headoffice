b5b111fb5b915ea993c23d5fee19a20341904cfa
孟凡玲 <mengfanling825@163.com>
2013-04-09 18:25:38 +0800

    remove relations

--- app/models/s_prod_mat_relation.rb
+++
@@ -1,7 +0,0 @@
-#encoding: utf-8
-class SProdMatRelation < ActiveRecord::Base
-  set_table_name :"lantan_db_all.prod_mat_relations"
-  set_primary_key "id"
-  belongs_to :s_material, :foreign_key => :material_id
-  belongs_to :s_product
-end
