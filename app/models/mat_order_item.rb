1c5d9693205b677290ab913b1820986e10da0d83
孟凡玲 <mengfanling825@163.com>
2013-04-09 14:13:55 +0800

    add s_material and s_pro_mat_relations

--- app/models/mat_order_item.rb
+++ app/models/mat_order_item.rb
@@ -2,6 +2,6 @@
 class MatOrderItem < ActiveRecord::Base
   set_table_name :"lantan_db_all.mat_order_items"
   set_primary_key "id"
-  belongs_to :material
+  belongs_to :s_material, :foreign_key => :material_id
   belongs_to :material_order
 end
