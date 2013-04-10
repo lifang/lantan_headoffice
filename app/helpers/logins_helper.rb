92d5ece52fa025c356c248142c507933bd422709
cloud <cloud@cloud-Sony.(none)>
2013-04-10 09:44:18 +0800

    modify bug

--- app/helpers/logins_helper.rb
+++ app/helpers/logins_helper.rb
@@ -1,13 +1,15 @@
 module LoginsHelper
   def get_menus(user_id)  #获取菜单
-    staff = Staff.find(user_id)
-    roles = staff.roles
-    menus = []
-    roles.each do |role|
-      role.menus.each do |m|
-        menus << m
-      end if !role.menus.blank?
-    end if !roles.blank?
-    @menus = menus.uniq
+    staff = Staff.find_by_id(user_id)
+    if !staff.nil?
+      roles = staff.roles
+      menus = []
+      roles.each do |role|
+        role.menus.each do |m|
+          menus << m
+        end if !role.menus.blank?
+      end if !roles.blank?
+      @menus = menus.uniq
+    end
   end
 end
