0f903926441fd16d404432f4b8abab314ec6cb79
孟凡玲 <mengfanling825@163.com>
2013-04-15 14:28:11 +0800

    when fahuo make a mat_out_order record

--- app/models/mat_out_order.rb
+++ app/models/mat_out_order.rb
@@ -11,22 +11,4 @@ class MatOutOrder < ActiveRecord::Base
                          :page => page,:per_page => per_page)
   end
 
-  def self.new_out_order selected_items,store_id,staff
-    status = 0
-    Material.transaction do
-      begin
-        (selected_items.split(",") || []).each do |item|
-          material = Material.find_by_id_and_store_id item.split("_")[0],store_id
-          if material
-            #出库记录 门店出库没有订单id和价格，并修改库存量
-            MatOutOrder.create(:material => material, :material_num => item.split("_")[1],:staff_id => staff)
-            material.update_attribute(:storage, material.storage - item.split("_")[1].to_i)
-          end
-        end
-      rescue
-        status = 1
-      end
-    end
-    status
-  end
 end
