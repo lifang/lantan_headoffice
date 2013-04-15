1ddf00c06855ec20f2eae25f72838a8373741529
孟凡玲 <mengfanling825@163.com>
2013-04-15 15:38:32 +0800

    remove unused method

--- app/models/mat_out_order.rb
+++ app/models/mat_out_order.rb
@@ -3,12 +3,4 @@ class MatOutOrder < ActiveRecord::Base
   belongs_to :material
   belongs_to :material_order
 
-  def self.out_list page,per_page, store_id
-    MatOutOrder.paginate(:select =>"m.*,o.material_num,s.name staff_name,o.price out_price,o.created_at out_time",
-                         :from => "mat_out_orders o",
-                         :joins => "inner join materials m on m.id=o.material_id inner join staffs s on s.id=o.staff_id",
-                         :conditions => "m.status=#{Material::STATUS[:NORMAL]} and m.store_id=#{store_id}",
-                         :page => page,:per_page => per_page)
-  end
-
 end
