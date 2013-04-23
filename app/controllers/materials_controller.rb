36d45bba85b12fb024789ed5a0dcf662163ddfdf
amanda <mengfanling825@163.com>
2013-04-23 21:22:19 +0800

    modify 已核实

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -9,12 +9,10 @@ class MaterialsController < ApplicationController   #库存控制器
     started_time = (params[:started_time].nil? || params[:started_time].empty?) ? "1 = 1" : "date_format(material_orders.created_at, '%Y-%m-%d') >= '#{params[:started_time]}'"
     ended_time = (params[:ended_time].nil? || params[:ended_time].empty?) ? "1 = 1" : "date_format(material_orders.created_at, '%Y-%m-%d') <= '#{params[:ended_time]}'"
 
-    @materials = Material.normal
-    .paginate(:page => params[:page] ||= 1, :per_page => Constant::PER_PAGE) if @tab.nil? || @tab.eql?("materials_tab")
+    @materials = Material.normal.paginate(:page => params[:page] ||= 1, :per_page => Constant::PER_PAGE) if @tab.nil? || @tab.eql?("materials_tab")
     @mat_out_orders = MatOutOrder.joins(:material).includes(:material).order("mat_out_orders.created_at desc").paginate(:page => params[:page] ||= 1, :per_page => Constant::PER_PAGE) if @tab.nil? || @tab.eql?("mat_out_tab")
     @mat_in_orders = MatInOrder.joins(:material).includes(:material).order("mat_in_orders.created_at desc").paginate(:page => params[:page] ||= 1 , :per_page => Constant::PER_PAGE) if @tab.nil? || @tab.eql?("mat_in_tab")
-    @mat_orders = MaterialOrder.joins(:mat_order_items => :material).includes(:mat_order_items => :material).is_headoffice_not_canceled.where(status).where(started_time).where(ended_time).order("material_orders.created_at desc").uniq
-    .paginate(:page => params[:page] ||= 1 , :per_page => Constant::PER_PAGE) if @tab.nil? || @tab.eql?("mat_orders_tab")
+    @mat_orders = MaterialOrder.joins(:mat_order_items => :material).includes(:mat_order_items => :material).is_headoffice_not_canceled.where(status).where(started_time).where(ended_time).order("material_orders.created_at desc").uniq.paginate(:page => params[:page] ||= 1 , :per_page => Constant::PER_PAGE) if @tab.nil? || @tab.eql?("mat_orders_tab")
     respond_to do |format|
       format.html
       format.js
@@ -45,12 +43,8 @@ class MaterialsController < ApplicationController   #库存控制器
 
   def material_check #库存核实
     material = Material.find(params[:m_id].to_i)
-    if material.storage == params[:storage].to_i
-      render :json => 0
-    else
-      material.update_attributes(:storage => params[:storage].to_i, :check_num => params[:storage].to_i)
-      render :json => 1
-    end
+    material.update_attributes(:storage => params[:storage].to_i, :check_num => 0)
+    render :json => 1
   end
   
   def show_material_order_beizhu #显示门店订货备注
