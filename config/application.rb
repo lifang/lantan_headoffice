b1ac80b7673cb79a6291473eb4ff50652e41f85f
lifang <santhe_2004@126.com>
2013-04-27 13:27:26 +0800

    update detial

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,9 @@ module LantanHeadoffice
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.time_zone ='Beijing'
+    config.active_record.default_timezone = :local
+    
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
